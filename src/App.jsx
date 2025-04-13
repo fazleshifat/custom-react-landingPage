c90024a18fbc0596241759095438e3cfb26d72d8
fazleshifat <mailbox189.shifat@gmail.com>
2025-04-14 00:23:10 +0600

    added design to rechart

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,11 @@ import DaisyNav from './Components/DaisyNav/DaisyNav'
 import Navbar from './Components/Navbar/Navbar'
 import PricingOptions from './Components/PricingOptions/PricingOptions'
 import ResultChart from './Components/ResultChart/ResultChart'
+import axios from 'axios'
+import MarksChart from './Components/MarksChart/MarksChart'
 
 const pricingPromise = fetch('pricingData.json').then(res => res.json());
+const marksPromise = axios.get('marksData.json')
 
 function App() {
 
@@ -21,6 +24,10 @@ function App() {
         </Suspense>
 
         <ResultChart></ResultChart>
+
+        <Suspense fallback={<span className="flex loading loading-dots text-blue-300 w-30 h-screen mx-auto"></span>}>
+          <MarksChart marksPromise={marksPromise}></MarksChart>
+        </Suspense>
       </main>
     </>
   )
